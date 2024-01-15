c9ef873a81f748a47679bd33cd411f4f300019f3
guttomarttins <guttomarttins@gmail.com>
2024-01-15 06:03:24 -0300

    add JPA e mapeando tabela produto

--- crud/src/main/java/br/com/gt/crud/model/Produto.java
+++ crud/src/main/java/br/com/gt/crud/model/Produto.java
@@ -1,5 +1,6 @@
 package br.com.gt.crud.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,12 +12,21 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "PRODUTO")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
     private Long id;
+    @Column(name = "DESCRICAO")
     private String descricao;
+    @Column(name = "MARCA")
     private String marca;
+    @Column(name="VALOR")
     private BigDecimal valor;
+    @Column(name = "VALIDADE")
     private LocalDate validade;
 
 }
